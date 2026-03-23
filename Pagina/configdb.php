cd15d349c0bd4244787c457e1dbe1acdb358f212
Yehu06 <yehublancopinilla.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 16:42:45 +0000

    Update configdb.php

--- Pagina/configdb.php
+++ Pagina/configdb.php
@@ -1,6 +1,6 @@
 <?php
-define("SERVIDOR",'localhost');
-define("USUARIO",'root');
-define("PASSWORD",'');
-define("BBDD",'jesuitas');
+define("SERVIDOR",'1daw.esvirgua.com');
+define("USUARIO",'user1daw_03');
+define("PASSWORD",'!%eHd4xC=-N&txG4');
+define("BBDD",'user1daw_BD1-03');
 ?>
